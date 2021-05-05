1a28bf20ea81e5aa89548ea057c4190aedd97486
sakib hasan <sakib686822@gmail.com>
2021-05-05 12:57:46 +0600

    running some shell Scripting

---
+++ ShellScripts/Script.sh
@@ -0,0 +1,16 @@
+echo "hi there"
+#bash variables
+name="sakib"
+echo $name
+
+#this is a bash funciton
+function greet(){
+echo "greeting for you $1"
+
+}
+#function with parameters
+greet $name
+
+
+#starting something new
+
---
+++ server.js
@@ -0,0 +1,5 @@
+for(i=0;i<30;i++){
+console.log(i);
+}
+
+
